c6dcd9fea191c21b6e0b6377647d1f4a5f388872
nitya <n.narasimhan@gmail.com>
2017-05-04 14:08:21 -0400

    3. Refactored codebase
    Minor cosmetic tweaks

--- react-fire/simple-app/src/index.js
+++ react-fire/simple-app/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
+
+import * as firebase from 'firebase';
+
+/*
+ * Add Firebase to your web app
+ */
+var config = {
+    apiKey: "AIzaSyB5ZbAuJCF0Q0EgI3hX6nKu3FvWwaJEzNg",
+    authDomain: "design-hawks-template.firebaseapp.com",
+    databaseURL: "https://design-hawks-template.firebaseio.com",
+    projectId: "design-hawks-template",
+    storageBucket: "design-hawks-template.appspot.com",
+    messagingSenderId: "100047492190"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
